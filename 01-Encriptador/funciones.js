a118b30549dea20d02cc43dcbe3cb9eb12579610
Amaury Ramírez <amauryr30@gmail.com>
2023-01-10 00:51:56 -0600

    agregando función 'Encriptar'

--- 01-Encriptador/funciones.js
+++ 01-Encriptador/funciones.js
@@ -1,5 +1,36 @@
 
 function encriptar(){
-    const mensaje = document.getElementById("mensaje").value;
-    alert(mensaje)
+    const mapa = new Map();
+    mapa.set("a","enter");
+    mapa.set("e","imes");
+    mapa.set("i","ai");
+    mapa.set("o","ober");
+    mapa.set("u","ufat");
+
+    var cadenafinal = "";
+    const entrada = document.getElementById("mensaje").value;
+    if (mensaje != ""){
+        var entradaSeparada = entrada.split("");
+        //console.log(entradaSeparada);
+
+        let iterador = 0;
+        for(iterador; iterador<entradaSeparada.length; iterador++){
+            element = entradaSeparada[iterador];
+            if ((element.charCodeAt(0) > 96) && (element.charCodeAt(0) < 123)){
+                if(mapa.has(element)){
+                    cadenafinal = cadenafinal + mapa.get(element);
+                }else{
+                    cadenafinal = cadenafinal + element;
+                }        
+                console.log("cadenaFinal= ",cadenafinal);
+
+            }else{
+                console.log("valor inválido");
+                alert("Solo letras minúsculas y sin acentos o caracteres especiales");
+                break;
+                
+            }
+        }   
+    }   
 }
+
