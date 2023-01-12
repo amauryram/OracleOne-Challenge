aa3b3d8687bb78e9b4d686bf2a5f8b3647d165fb
Amaury Ramírez <amauryr30@gmail.com>
2023-01-11 21:35:12 -0600

    Función desencriptar agregada, salida en consola.

--- 01-Encriptador/funciones.js
+++ 01-Encriptador/funciones.js
@@ -1,9 +1,40 @@
 
+
+function desencriptar(){
+    const mapa = new Map();
+    mapa.set("e",4);
+    mapa.set("i",3);
+    mapa.set("a",1);
+    mapa.set("o",3);
+    mapa.set("u",3);
+
+    var cadenaResultado = ""; 
+    const mensaje = document.getElementById("mensaje").value;
+    if(mensaje != ""){
+        var entradaSeparada = mensaje.split("");
+        let iterador = 0;
+
+        while(iterador<entradaSeparada.length){
+            element = entradaSeparada[iterador];
+            if(mapa.has(element)){
+                cadenaResultado = cadenaResultado + element;
+                iterador += 1 + mapa.get(element);
+
+            }else{
+                cadenaResultado = cadenaResultado + element;
+                iterador++;
+            }
+        }
+        console.log(cadenaResultado);
+        
+    } 
+}
+
 function encriptar(){
     const mapa = new Map();
-    mapa.set("a","enter");
-    mapa.set("e","imes");
-    mapa.set("i","ai");
+    mapa.set("e","enter");
+    mapa.set("i","imes");
+    mapa.set("a","ai");
     mapa.set("o","ober");
     mapa.set("u","ufat");
 
@@ -11,7 +42,6 @@ function encriptar(){
     const entrada = document.getElementById("mensaje").value;
     if (mensaje != ""){
         var entradaSeparada = entrada.split("");
-        //console.log(entradaSeparada);
 
         let iterador = 0;
         for(iterador; iterador<entradaSeparada.length; iterador++){
