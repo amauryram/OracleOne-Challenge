dff4e1af4719de54feaeeffc37336f45ad23db80
Amaury Ramírez <amauryr30@gmail.com>
2023-01-14 15:52:18 -0600

    agregando funcionalidad de copypaste en boton 'copiar'

--- 01-Encriptador/funciones.js
+++ 01-Encriptador/funciones.js
@@ -1,4 +1,17 @@
+function copiarPortapapeles(){
+    //console.log("Entrando a copiar");
+    var copiar = document.getElementById('muestraMensaje').innerHTML;
 
+    //var content = document.getElementById('textArea').innerHTML;
+    navigator.clipboard.writeText(copiar)
+        .then(() => {
+        console.log("Texto copiado exitosamente...")
+    })
+        .catch(err => {
+        console.log('Hubo un error al copiar', err);
+    })
+ 
+}
 
 function desencriptar(){
     const mapa = new Map();
@@ -10,6 +23,8 @@ function desencriptar(){
 
     var cadenaResultado = ""; 
     const mensaje = document.getElementById("mensaje").value;
+    const avisoid = document.getElementById("avisoid");
+    const mensajefinal = document.getElementById("muestraMensaje");
     if(mensaje != ""){
         var entradaSeparada = mensaje.split("");
         let iterador = 0;
@@ -26,6 +41,8 @@ function desencriptar(){
             }
         }
         console.log(cadenaResultado);
+        avisoid.innerHTML = "El mensaje Desencriptadoe es:";
+        mensajefinal.innerHTML = cadenaResultado;
         
     } 
 }
@@ -40,19 +57,27 @@ function encriptar(){
 
     var cadenafinal = "";
     const entrada = document.getElementById("mensaje").value;
+    const avisoid = document.getElementById("avisoid");
+    const mensajefinal = document.getElementById("muestraMensaje");
+    const btnCopiar = document.getElementById("btnCopiar");
     if (mensaje != ""){
         var entradaSeparada = entrada.split("");
 
         let iterador = 0;
         for(iterador; iterador<entradaSeparada.length; iterador++){
             element = entradaSeparada[iterador];
-            if ((element.charCodeAt(0) > 96) && (element.charCodeAt(0) < 123)){
+            if (((element.charCodeAt(0) > 96) && (element.charCodeAt(0) < 123))||(element.charCodeAt(0) == 32)){
                 if(mapa.has(element)){
                     cadenafinal = cadenafinal + mapa.get(element);
                 }else{
                     cadenafinal = cadenafinal + element;
                 }        
                 console.log("cadenaFinal= ",cadenafinal);
+                avisoid.innerHTML = "El mensaje Encriptado es: ";
+                mensajefinal.innerHTML = cadenafinal;
+                btnCopiar.style.display = null;
+                btnCopiar.style.display = "initial";
+
 
             }else{
                 console.log("valor inválido");
